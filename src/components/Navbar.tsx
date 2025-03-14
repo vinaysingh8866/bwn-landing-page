8d79c56cbcf7737d5d6af8651c3a11386d604a66
Vinay Singh <vinaysingh8866@gmail.com>
2025-03-14 13:00:30 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -59,7 +59,7 @@ export default function Navbar({}:NavbarProps ){
                             p="0"
                             onClick={() => {
                                 // goto public/installers/unsigned_bwn.exe
-                                window.location.href = "https://purple-abundant-anaconda-910.mypinata.cloud/ipfs/QmbChqPoTTTNdam5G5pfZyKK7m2CMw2qzAQ2nXf9o74UJW/BWN.exe";
+                                window.location.href = "https://raw.githubusercontent.com/vinaysingh8866/bwn-landing-page/main/public/installers/BWN.exe";
                             }}
                             >
                             <Image src="/download.svg" h={"16px"} />
